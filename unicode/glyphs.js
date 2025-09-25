c42c71ecadbed6e642a754cefc806196b6f0e955
NOVEYA <ngoisigma@gmail.com>
2025-09-25 22:41:38 +0300

    Create glyphs.js

---
+++ unicode/glyphs.js
@@ -0,0 +1,31 @@
+{
+  "version": "0.1",
+  "namespace": "Σ‑FDL",
+  "glyphs": [
+    {
+      "name": "NORMA_TIME",
+      "char": "⟲",
+      "semantics": "norm/time"
+    },
+    {
+      "name": "NORMA_ENERGY",
+      "char": "⚡",
+      "semantics": "norm/energy"
+    },
+    {
+      "name": "NORMA_MEMORY",
+      "char": "✸",
+      "semantics": "norm/memory"
+    },
+    {
+      "name": "NORMA_ATTN",
+      "char": "◴",
+      "semantics": "norm/attention"
+    },
+    {
+      "name": "NORMA_FLOW",
+      "char": "⇶",
+      "semantics": "norm/flow"
+    }
+  ]
+}
